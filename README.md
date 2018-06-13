c9695b1ef3c7f30b77dfaa5943699653e8e729ce
M <m@cognusion.com>
2018-06-13 11:06:19 -0400

    Releases a trivial library from my personal repo

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
+
 # go-timings
+
+[![GoDoc](https://godoc.org/github.com/cognusion/go-timings?status.svg)](https://godoc.org/github.com/cognusion/go-timings)
+
+Several helpers for timing functions or sections of functions. e.g.
+
+```go
+func MyFunc() {
+  defer timings.Track("MyFunc", time.Now(), someLogger)
+  // Do stuff here
+  
+}
+```
---
+++ timings.go
@@ -0,0 +1,65 @@
+package timings
+
+import (
+	"log"
+	"os"
+	"time"
+)
+
+var (
+	defaultTracker Tracker
+)
+
+// Tracker is a simple device to track timings
+type Tracker struct {
+	start time.Time
+	end   time.Time
+}
+
+// Start records the start time
+func (t *Tracker) Start() {
+	t.start = time.Now()
+}
+
+// Stop records the end time
+func (t *Tracker) Stop() {
+	t.end = time.Now()
+}
+
+// Since returns a Duration since the start time
+func (t *Tracker) Since() time.Duration {
+	return time.Since(t.start)
+}
+
+// Duration returns the Duration of time between Start() and Stop()
+func (t *Tracker) Duration() time.Duration {
+	return t.end.Sub(t.start)
+}
+
+// Track tracks how long a function takes, to a log.Logger
+// e.g. make the first line
+// defer Track("func_name", time.Now(), logger).
+func Track(name string, start time.Time, l *log.Logger) {
+	elapsed := time.Since(start)
+	l.Printf("%s took %s", name, elapsed)
+}
+
+// TrackOut tracks how long a function takes, to Stdout
+// e.g. make the first line:
+// defer TrackOut("func_name", time.Now()).
+func TrackOut(name string, start time.Time) {
+	Track(name, start, log.New(os.Stdout, "[TIMING]", log.Lshortfile))
+}
+
+// TrackErr tracks how long a function takes, to Stderr
+// e.g. make the first line:
+// defer Trackerr("func_name", time.Now()).
+func TrackErr(name string, start time.Time) {
+	Track(name, start, log.New(os.Stderr, "[TIMING]", log.Lshortfile))
+}
+
+// Now is equivalent to time.Now(), but saves the import of "time" if
+// it's not otherwise needed
+func Now() time.Time {
+	return time.Now()
+}
